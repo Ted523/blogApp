5c1f3477ab2edb2623109497c7e29fc0386b34c9
Ted523 <65559468+Ted523@users.noreply.github.com>
2021-08-19 23:40:03 +0300

    wip-home-template-design preliminary1

--- blogApp/blogApplication/views.py
+++ blogApp/blogApplication/views.py
@@ -1,13 +1,42 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+posts = [
+    {
+        'author': 'Teddy Otieno',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 19, 2021'
+    },
+    
+    {
+        'author': 'Nova Waithaka',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'July 19, 2021'
+    },
 
-import datetime
+    {
+        'author': 'Bridgit Wanyama',
+        'title': 'Blog Post 3',
+        'content': 'Third post content',
+        'date_posted': 'August 19, 2021'
+    },
+
+    {
+        'author': 'Mercy Olorontubi',
+        'title': 'Blog Post 4',
+        'content': 'Fourth post content',
+        'date_posted': 'September 19, 2021'
+    },
+]
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'home.html', context)
 
 def create(request):
     return render(request, 'create_post.html')
