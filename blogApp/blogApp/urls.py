4e2363b32938ef9bca5dbe44d46c5094d6a3f730
Ted523 <65559468+Ted523@users.noreply.github.com>
2022-02-16 00:07:15 +0300

    Login, Logout & Password Reset Functionality

--- blogApp/blogApp/urls.py
+++ blogApp/blogApp/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from Accounts import views as account_views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', account_views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='Accounts/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='Accounts/logout.html'), name="logout"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='Accounts/password_reset.html'), name="password_reset"),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='Accounts/password_reset_done.html'), name="password_reset_done"),
+
     path('', include("blogApplication.urls")),	
 ]
